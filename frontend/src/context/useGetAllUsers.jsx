cf48458835c02d60b586145ab0eb422a82476be9
pushkar <pushkarmoudgil01@gmail.com>
2024-10-04 03:20:18 +0530

    show users in frontend

---
+++ frontend/src/context/useGetAllUsers.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import Cookies from "js-cookie"
+
+function useGetAllUsers() {
+  const [allUsers,setAllUsers]=useState([])
+  const [loading,setLoading]=useState(false)
+
+
+  useEffect(()=>{ 
+    const getUsers=async()=>{
+   setLoading(true)
+        try{
+       const token=Cookies.get("jwt")
+       
+
+       const response =await axios.get("http://localhost:3000/user/allusers",{
+        credentials:"include",
+        headers:{
+            Authorization:`Bearer ${token}`
+        }
+       })
+       setAllUsers(response.data);
+       setLoading(false)
+    
+        }catch(error){
+            console.log("Error in useGetAllUsers:"+ error)
+        }
+
+  }
+   
+ getUsers();
+  },[])
+  return[allUsers,loading]
+}
+
+export default useGetAllUsers
